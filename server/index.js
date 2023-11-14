a433f37ddfd5b81945786e58c160072c1c85d0bd
mmplanch <mmplanch@ncsu.edu>
2023-11-14 09:44:36 -0500

    got prettier working

--- server/index.js
+++ server/index.js
@@ -5,5 +5,5 @@ const PORT = process.env.PORT || 3001;
 const app = express();
 
 app.listen(PORT, () => {
-console.log(`Server listening on ${PORT}`);
+  console.log(`Server listening on ${PORT}`);
 });
