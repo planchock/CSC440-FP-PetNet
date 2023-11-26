1b256f3793717d608b3e0a27085d1f77282ed149
Cameron Seitz <cdseitz@ncsu.edu>
2023-11-26 12:19:33 -0500

    added .env and changed some col names

--- database/db_schema/petnet.sql
+++ database/db_schema/petnet.sql
@@ -11,9 +11,9 @@ CREATE TABLE IF NOT EXISTS `user` (
   `username` varchar(50) NOT NULL,
   `password` varchar(50) NOT NULL,
   `salt` varchar(50) NOT NULL,
-  `media_id` int(11),
+  `profile_pic` int(11),
   PRIMARY KEY (`user_id`),
-  CONSTRAINT `FK_MEDIA_ID`FOREIGN KEY (`media_id`) REFERENCES `media` (`media_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+  CONSTRAINT `FK_PROFILE_PIC`FOREIGN KEY (`profile_pic`) REFERENCES `media` (`media_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `follow` (
@@ -35,13 +35,13 @@ CREATE TABLE IF NOT EXISTS `pet` (
 
 CREATE TABLE IF NOT EXISTS `group` (
   `group_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
-  `media_id` int(11),
+  `group_pic` int(11),
   `group_name` varchar(50),
   `group_desc` varchar(255),
   `admin_id` int(11) unsigned NOT NULL,
   PRIMARY KEY (`group_id`),
-  CONSTRAINT `FK_ADMIN_ID`FOREIGN KEY (`user_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
-  CONSTRAINT `FK_MEDIA_ID`FOREIGN KEY (`media_id`) REFERENCES `media` (`media_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_ADMIN_ID`FOREIGN KEY (`admin_id`) REFERENCES `user` (`user_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `FK_GROUP_PIC`FOREIGN KEY (`group_pic`) REFERENCES `media` (`media_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
 
 CREATE TABLE IF NOT EXISTS `group_member` (
