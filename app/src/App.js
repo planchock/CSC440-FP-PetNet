68e315b77dd577ae58f0f2bf52f79f28823d6a26
Cade Cuddy <cadecuddy@gmail.com>
2023-11-26 19:10:28 -0500

    Complete landing page

--- app/src/App.js
+++ app/src/App.js
@@ -1,9 +1,19 @@
-import 'tailwindcss/tailwind.css'; 
+import "tailwindcss/tailwind.css";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Landing from "./Pages/Landing";
+import Login from "./Pages/Login";
+import Signup from "./Pages/Signup";
 
 function App() {
   return (
-    <div className="min-h-screen flex items-center justify-center bg-gradient-to-r from-cyan-500 to-blue-500">
-      <p>Empty</p>
+    <div className="min-h-screen bg-gradient-to-r from-indigo-200 via-red-200 to-yellow-100">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Landing />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/signup" element={<Signup />} />
+        </Routes>
+      </Router>
     </div>
   );
 }
---
+++ app/src/Pages/Landing.js
@@ -0,0 +1,50 @@
+import "tailwindcss/tailwind.css";
+
+function Landing() {
+  return (
+    <div className="flex items-center justify-center max-w-5xl min-h-screen mx-auto">
+      <div className="flex items-center space-x-40">
+        <div className="block">
+          <h1 className="text-6xl font-extrabold text-black">
+            Show off
+            <br /> your pets.
+          </h1>
+          <h2 className="mt-4 text-3xl font-bold text-neutral-50 drop-shadow-lg">
+            Join the community.
+          </h2>
+          <div className="flex mt-2 space-x-4">
+            <a href="signup" class="relative inline-block text-lg group">
+              <span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+                <span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-pink-100"></span>
+                <span class="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+                <span class="relative">Join Now</span>
+              </span>
+              <span
+                class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+                data-rounded="rounded-lg"
+              ></span>
+            </a>
+            <a href="login" class="relative inline-block text-lg group">
+              <span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
+                <span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-yellow-200"></span>
+                <span class="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
+                <span class="relative">Log In</span>
+              </span>
+              <span
+                class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
+                data-rounded="rounded-lg"
+              ></span>
+            </a>
+          </div>
+        </div>
+        <img
+          src="https://static.vecteezy.com/system/resources/previews/018/871/732/original/cute-and-happy-dog-png.png"
+          height={300}
+          width={500}
+        />
+      </div>
+    </div>
+  );
+}
+
+export default Landing;
