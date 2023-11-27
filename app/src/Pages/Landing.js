78b830d76589520533a4d1c91be233a8e03c5868
Cade Cuddy <cadecuddy@gmail.com>
2023-11-26 19:17:41 -0500

    Change <a> to router Link component

--- app/src/Pages/Landing.js
+++ app/src/Pages/Landing.js
@@ -1,8 +1,9 @@
 import "tailwindcss/tailwind.css";
+import { Link } from "react-router-dom";
 
 function Landing() {
   return (
-    <div className="flex items-center justify-center max-w-5xl min-h-screen mx-auto">
+    <div className="flex items-center justify-center max-w-5xl mx-auto">
       <div className="flex items-center space-x-40">
         <div className="block">
           <h1 className="text-6xl font-extrabold text-black">
@@ -13,7 +14,7 @@ function Landing() {
             Join the community.
           </h2>
           <div className="flex mt-2 space-x-4">
-            <a href="signup" class="relative inline-block text-lg group">
+            <Link to="/signup" class="relative inline-block text-lg group">
               <span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
                 <span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-pink-100"></span>
                 <span class="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
@@ -23,8 +24,8 @@ function Landing() {
                 class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
                 data-rounded="rounded-lg"
               ></span>
-            </a>
-            <a href="login" class="relative inline-block text-lg group">
+            </Link>
+            <Link to="/login" class="relative inline-block text-lg group">
               <span class="relative z-10 block px-5 py-3 overflow-hidden font-medium leading-tight text-gray-800 transition-colors duration-300 ease-out border-2 border-gray-900 rounded-lg group-hover:text-white">
                 <span class="absolute inset-0 w-full h-full px-5 py-3 rounded-lg bg-yellow-200"></span>
                 <span class="absolute left-0 w-48 h-48 -ml-2 transition-all duration-300 origin-top-right -rotate-90 -translate-x-full translate-y-12 bg-gray-900 group-hover:-rotate-180 ease"></span>
@@ -34,10 +35,10 @@ function Landing() {
                 class="absolute bottom-0 right-0 w-full h-12 -mb-1 -mr-1 transition-all duration-200 ease-linear bg-gray-900 rounded-lg group-hover:mb-0 group-hover:mr-0"
                 data-rounded="rounded-lg"
               ></span>
-            </a>
+            </Link>
           </div>
         </div>
-        <img src="/dog.webp" height={300} width={500} />
+        <img src="/dog.webp" height={300} width={450} />
       </div>
     </div>
   );
